dfacc86463178acd2d03ef9edd6b2f14323abcd9
Yoga0702 <humnabadyogananda07022000@gmail.com>
2024-03-11 12:30:05 +0530

    Ecommerce

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Everlane sells high-end, eco-conscious fashion and accessories for men and women. Their product descriptions match their style: sassy, yet classy. Here’s a look at The Day Heel that retails for $145 online:[*]What I love about Everlane and the way they approach describing this heel (and other products) is that they speak in the language of their customers.Using phrases like “All. Damn. Day.” and “take your usual size” makes shoppers feel as if they were talking to their friends, not a business.</p>
+            <p className=''>
+
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
